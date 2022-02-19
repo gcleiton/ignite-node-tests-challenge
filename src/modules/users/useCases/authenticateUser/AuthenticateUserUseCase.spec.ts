615a727ea7a0b6c490e22031c0589fc9f62de7e6
Gabriel Cleiton <gcleiton.dev@gmail.com>
2022-02-19 16:56:23 -0300

    test: ensure AuthenticateUserUseCase throws IncorrectEmailOrPasswordError if password not match

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -3,6 +3,14 @@ import { IUsersRepository } from "../../repositories/IUsersRepository"
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase"
 import { IncorrectEmailOrPasswordError } from "./IncorrectEmailOrPasswordError"
 
+const mockFakeUser = () => {
+  return {
+    name: 'any_name',
+    email: 'any_email',
+    password: 'any_password'
+  }
+}
+
 describe('AuthenticateUserUseCase', () => {
   let userRepository: IUsersRepository
   let sut: AuthenticateUserUseCase
@@ -14,6 +22,14 @@ describe('AuthenticateUserUseCase', () => {
 
   it('should throw IncorrectEmailOrPasswordError if user not exists', async () => {
     const promise = sut.execute({email: 'any_email', password: 'any_password'})
+
+    await expect(promise).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError)
+  })
+
+  it('should throw IncorrectEmailOrPasswordError if password not match', async () => {
+    userRepository.findByEmail = jest.fn().mockResolvedValueOnce(mockFakeUser())
+
+    const promise = sut.execute({email: 'any_email', password: 'invalid_password'})
 
     await expect(promise).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError)
   })
