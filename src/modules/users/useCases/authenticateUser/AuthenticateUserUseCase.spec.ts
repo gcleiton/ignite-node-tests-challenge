2224df38028eac4a304139bf1d723126a425b7aa
Gabriel Cleiton <gcleiton.dev@gmail.com>
2022-02-19 17:33:29 -0300

    test: ensure AuthenticateUserUseCase be able to authenticate an user

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,10 +1,15 @@
+import bcryptjs from 'bcryptjs'
+
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository"
 import { IUsersRepository } from "../../repositories/IUsersRepository"
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase"
 import { IncorrectEmailOrPasswordError } from "./IncorrectEmailOrPasswordError"
 
+jest.mock('bcryptjs')
+
 const mockFakeUser = () => {
   return {
+    id: 'any_id',
     name: 'any_name',
     email: 'any_email',
     password: 'any_password'
@@ -17,20 +22,35 @@ describe('AuthenticateUserUseCase', () => {
 
   beforeEach(() => {
     userRepository = new InMemoryUsersRepository()
+    userRepository.findByEmail = jest.fn().mockResolvedValue(mockFakeUser())
+    bcryptjs.compare = jest.fn().mockResolvedValue(true)
+
     sut = new AuthenticateUserUseCase(userRepository)
   })
 
   it('should throw IncorrectEmailOrPasswordError if user not exists', async () => {
+    userRepository.findByEmail = jest.fn().mockResolvedValue(undefined)
     const promise = sut.execute({email: 'any_email', password: 'any_password'})
 
     await expect(promise).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError)
   })
 
   it('should throw IncorrectEmailOrPasswordError if password not match', async () => {
-    userRepository.findByEmail = jest.fn().mockResolvedValueOnce(mockFakeUser())
+    bcryptjs.compare = jest.fn().mockResolvedValue(false)
 
     const promise = sut.execute({email: 'any_email', password: 'invalid_password'})
 
     await expect(promise).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError)
+  })
+
+  it('should be able to authenticate an user', async () => {
+    const result = await sut.execute({email: 'any_email', password: 'any_password'})
+
+    expect(result.user).toEqual({
+      id: mockFakeUser().id,
+      name: mockFakeUser().name,
+      email: mockFakeUser().email,
+    })
+    expect(result.token).toBeDefined()
   })
 })
